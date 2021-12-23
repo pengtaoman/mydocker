c60b1007a5c6453a2e31c77c25b18c6c04ee22b7
pengtaoman <pengtaoman@163.com>
2021-12-23 20:40:09 +0800

    颠三倒四的

---
+++ main-test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+)
+
+func main() {
+
+	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	log.Println("##############################################")
+}
